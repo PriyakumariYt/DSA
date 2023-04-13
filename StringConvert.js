fdc899c39a1b9ed597df3abbc2f766bf29d19ef0
Priya Kumari <127418517+PriyakumariYt@users.noreply.github.com>
2023-04-13 09:45:10 +0530

    upload

--- StringConvert.js
+++ StringConvert.js
@@ -1,14 +1,30 @@
-var str = "aBbcd"
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lower = "abcdefghijklmnopqrstuvwxyz"
-var res = ""
-for(let i = 0; i<str.length;i++){
-    for(let j = 0; j<upper.length; j++){
-        if(str[i]==upper[j]){
-            res+= lower[j]
-        }else if(str[i]==lower[j]){
-            res+= upper[j]
-        }
+
+var str = "TeStInG";
+var upper = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+var lower = `abcdefghijklmnopqrstuvwxyz`;
+var res = "";
+for (let i = 0; i < str.length; i++) {
+  for (let j = 0; j < 26; j++) {
+    if (str[i] == upper[j]) {
+      res += lower[j];
+    } else if (str[i] == lower[j]) {
+      res += upper[j];
     }
+  }
 }
-console.log(res)
+console.log(res);
+
+var str = "RefLEctION";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var res = "";
+for (let i = 0; i < str.length; i++) {
+  for (let j = 0; j < 26; j++) {
+    if (str[i] === lowerCase[j]) {
+      res += upperCase[j];
+    } else if (str[i] === upperCase[j]) {
+      res += lowerCase[j];
+    }
+  }
+}
+console.log(res);
